5f8e1ee3ddd80f23a7b94c464455c0e0f47efd5d
Hideki Yamada <hideki9th@gmail.com>
2024-03-17 11:53:41 +0900

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import tldextract
+import yaml
 import collections
-import os
+import sys
 import pprint
 
 
@@ -10,21 +11,52 @@ import pprint
 #=============================================================#
 
 class SurfaceCSV:
+    def __init__(self, csv_type, csv_path, tgt_col_list):
+        self.csv_type = csv_type
+        self.tgt_col_list = tgt_col_list
 
-    def __init__(self, csv_path):
+        # Declare Instance variables. This class can work without it, but it is for easy to understand.
+        self.df=pd.DataFrame()
+        self.result_data = {} # Summarized data. The structure is dict in dict
+
+        self.readCSV(csv_path)
+        self.analyze()
+
+
+    def readCSV(self, csv_path):
         try:
             self.df = pd.read_csv(csv_path)
         except FileNotFoundError as e:
             exit(f'Error - {e}')
 
-    #特定カラムの値を取得（CSVを縦に取得）
-    #dfを指定したい場合があるため、空のDataFrameを作成し、df.emptyで判断する。本当は引数にdf = self.dfを入れたいが、こうしないとエラーになる。
-    def getSeries(self, col, df=pd.DataFrame()): 
+    def analyze(self):
+        for column in self.tgt_col_list:
+            if(column == 'Issue ID'): #for 'Issue ID' in issue.csv
+                severity_list = ['critical', 'high', 'medium', 'low']
+                for severity in severity_list:
+                    issue_dict = self.getIssues(severity)
+                    str = f'{severity} {column}'
+                    self.result_data[str] = issue_dict
+
+            elif(column == 'RootDomain'): # To extract root domain from 'Domains' from domain.csv and service.csv.
+                col_dict = self.getRootDomainCnt()
+                self.result_data[column] = col_dict
+
+
+            else:
+                col_dict = self.getColCounts(column)
+                self.result_data[column] = col_dict
+
+
+    # Get the value of a specific column (get values vertically)
+    # Since there are cases where you want to specify df, create an empty DataFrame and use df.empty to determine it. 
+    # I wanted to put "df = self.df" in the argument, but python does not allow it.
+    def getSeries(self, column, df=pd.DataFrame()): 
         if(df.empty):
             df = self.df
 
         item_list = []
-        for item in df[col]:
+        for item in df[column]:
             if(pd.isnull(item)):
                 continue
             item_list.append(item)
@@ -32,13 +64,13 @@ class SurfaceCSV:
         return item_list
 
 
-    #一般的な項目の数を取得
-    def getColCounts(self, col):
-        col_list = self.getSeries(col)
-        return dict(collections.Counter(col_list))
+    # Get number of generic column
+    def getColCounts(self, column):
+        column_list = self.getSeries(column)
+        return dict(collections.Counter(column_list))
 
 
-    #Root Domainの数を取得
+    # Get number of root domain
     def getRootDomainCnt(self):
         domain_list = []
         fqdn_list = self.getSeries('Domain')
@@ -49,14 +81,14 @@ class SurfaceCSV:
         return dict(collections.Counter(domain_list))
 
 
-    # Issueの一覧を取得
+    # Get issue list
     def getIssues(self, severity):
         df_per_severity = self.df[self.df['Severity'] == severity]
     
         issue_list = self.getSeries('Issue ID', df_per_severity)
         issue_cve_list = []
         issue_non_cve_list = []
-        #CVEとNon-CVEの出力を分離する。分かりやすくするため。
+        # Separate CVE and Non-CVE for easy to understand.
         for issue in issue_list:
             if(issue.startswith('CVE')):
                 issue_cve_list.append(issue)
@@ -66,60 +98,15 @@ class SurfaceCSV:
         issue_list = issue_cve_list + issue_non_cve_list
         return dict(collections.Counter(issue_list))
 
+    # This method will support multiple type of output in the future.
+    def print(self, type='csv'):
+        if(type == 'csv'):
+            for column, col_dict in self.result_data.items():
+                title = f'# {column} counts - from {self.csv_type}.csv'
 
-
-# This class will support multiple output format.
-class OutputFormat:
-    def __init__(self, title, dict):
-        self.title = title
-        self.dict = dict
-
-    def print2Console(self):
-        print('\n')
-        print(self.title)
-        printDict(self.dict)
-
-
-
-class AnalyzeRouter:
-    def __init__(self, csv_type, tgt_col_list, output_fmt):
-        self.csv_type = csv_type
-        self.tgt_col_list = tgt_col_list
-        self.output_fmt = output_fmt
-        self.routing()
-    
-    def routing(self):
-        if(self.csv_type == 'domain'):
-            sc = SurfaceCSV(csv_domain)
-        elif(self.csv_type == 'service'):
-            sc = SurfaceCSV(csv_service)
-        elif(self.csv_type == 'asset'):
-            sc = SurfaceCSV(csv_asset)
-        elif(self.csv_type == 'issue'):
-            sc = SurfaceCSV(csv_issue)
-
-        for column in self.tgt_col_list:
-            if(column == 'Issue ID'): #for 'Issue ID' in issue.csv
-                severity_list = ['critical', 'high', 'medium', 'low']
-                for severity in severity_list:
-                    issue_dict = sc.getIssues(severity)
-                    self.output(f'{severity} {column}', issue_dict)
-                continue
-
-            elif(column == 'RootDomain'): # To extract root domain from 'Domains' from domain.csv and service.csv.
-                col_dict = sc.getRootDomainCnt()
-
-            else:
-                col_dict = sc.getColCounts(column)
-            
-            self.output(column, col_dict)
-
-
-
-    def output(self, column, col_dict):
-        if(self.output_fmt == 'console_csv'):
-            of = OutputFormat(f'# {column} counts - from {self.csv_type}.csv', col_dict)
-            of.print2Console()
+                print('\n')
+                print(title)
+                printDict(col_dict)
 
 
 
@@ -134,27 +121,29 @@ def printDict(dict):
 #=============================================================#
 # main
 #=============================================================#
-# config
+
+# Target column
 domain_tgt_col_list = ['RootDomain', 'Domain'] #RootDomain has special culculation
 service_tgt_col_list = ['RootDomain', 'Protocol', 'Port', 'Platform']
 asset_tgt_col_list = ['Hosting Provider', 'Country Name']
 issue_tgt_col_list = ['Issue ID'] #Issue ID has special culculation
-output_fmt = 'console_csv'
 
 
-# Get environment variables
+# Read config file
 try:
-    csv_domain = os.environ['SURFACE_CSV_DOMAIN']
-    csv_service = os.environ['SURFACE_CSV_SERVICE']
-    csv_asset = os.environ['SURFACE_CSV_ASSET']
-    csv_issue = os.environ['SURFACE_CSV_ISSUE']
-except KeyError as e:
-    exit(f'Error - Please specify CSV file path in environment variable: {e}')
+    with open(sys.argv[1], 'r') as yml:
+        config = yaml.safe_load(yml)
+except yaml.YAMLError as e:
+    exit(f'Error - {e}')
 
 
-# Analyze
-AnalyzeRouter('domain', domain_tgt_col_list, output_fmt)
-AnalyzeRouter('service', service_tgt_col_list, output_fmt)
-AnalyzeRouter('asset', asset_tgt_col_list, output_fmt)
-AnalyzeRouter('issue', issue_tgt_col_list, output_fmt)
+# Analyze each CSV
+sc = SurfaceCSV('domain', config['SurfaceCSVPath']['domain'], domain_tgt_col_list)
+sc.print()
+sc = SurfaceCSV('service', config['SurfaceCSVPath']['service'], service_tgt_col_list)
+sc.print()
+sc = SurfaceCSV('asset', config['SurfaceCSVPath']['asset'], asset_tgt_col_list)
+sc.print()
+sc = SurfaceCSV('issue', config['SurfaceCSVPath']['issue'], issue_tgt_col_list)
+sc.print()
 
